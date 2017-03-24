c9f07e590cc3c72bafa7e6bbc59e5e01b47f07dd
Your Name <yarik.vashchuk@gmail.com>
2017-03-24 20:47:38 +0200

    socket.io recconect webchat

--- public/js/websockets.js
+++ public/js/websockets.js
@@ -2,22 +2,39 @@
   const form = $('form[name="webchat"]');
   const input = $('form[name="webchat"] input');
   const ul = $('form[name="webchat"] ul');
-  const socket = io('', {});
+
+  const socket = io('', {
+    'reconnectionAttempts': 30,
+    'reconnectionDelay': 500
+  });
 
   socket
     .on('message', function(username, message) {
-      printMessage(message);
+      printMessage('> ' + message);
     })
     .on('connect', function() {
-      printStatus("соединение установлено", "#d6f1e9");
+      printStatus("Соединение установлено", "#d6f1e9");
       form.on('submit', sendMessage);
       input.prop('disabled', false);
     })
     .on('disconnect', function() {
-      printStatus("соединение потеряно", "#f1d6ee");
+      printStatus("Соединение потеряно", "#f1d6ee");
+      printStatus("Пееродключение...", "#f1d6ee");
       form.off('submit', sendMessage);
       input.prop('disabled', true);
     })
+    .on('logout', function() {
+      location.href = "/";
+     })
+    .on('error', function(reason) {
+      if (reason == "handshake unauthorized") {
+        printStatus("вы вышли из сайта");
+      } 
+    })
+    .on('reconnect_failed', function() {
+      printStatus("Соединение разорвано");
+      console.log('connection lost');
+    });
 
   const sendMessage = function() {
     const text = input.val();
@@ -39,5 +56,4 @@
     .addClass('log')
     .text(status)).appendTo(ul);
   }
-
 })();
