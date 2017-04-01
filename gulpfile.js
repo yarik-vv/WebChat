4ddb1dfd378e0b38a5572be1c35984f8da8d6ceb
Your Name <yarik.vashchuk@gmail.com>
2017-04-01 09:18:44 +0300

    gulp build css and js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,22 @@
 const gulp = require('gulp');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
-gulp.task('build', function() {
-  return gulp.src('frontend/**/*.*')
+var rename = require("gulp-rename");
+
+gulp.task('css', function() {
+  return gulp.src('frontend/**/*.css')
+    .pipe(rename({
+      dirname: "css"
+    }))
     .pipe(gulp.dest('public'));
 });
+
+gulp.task('js', function() {
+  return gulp.src('frontend/**/*.js')
+    .pipe(rename({
+      dirname: "js"
+    }))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series('css', 'js'));
