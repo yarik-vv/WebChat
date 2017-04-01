948b52f3d9af877fae6df16baa715ce4a7f95503
Your Name <yarik.vashchuk@gmail.com>
2017-04-01 09:42:59 +0300

    clean task, templates build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,23 @@ const gulp = require('gulp');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 var rename = require("gulp-rename");
+const del = require('del');
+
+gulp.task('clean', function() {
+  return del([
+    'views',
+    'public/css', 
+    'public/js'
+  ]);
+});
+
+gulp.task('templates', function() {
+  return gulp.src('frontend/**/*.jade')
+    .pipe(rename({
+      dirname: "views"
+    }))
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('css', function() {
   return gulp.src('frontend/**/*.css')
@@ -21,4 +38,4 @@ gulp.task('js', function() {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', gulp.series('css', 'js'));
+gulp.task('build', gulp.series('clean', 'templates', 'css', 'js'));
