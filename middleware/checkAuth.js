a20bb73e1bf08e7a1716316e7722f124391859ae
Your Name <yarik.vashchuk@gmail.com>
2017-04-18 00:51:35 +0300

    has created error page

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,7 +3,7 @@ const HttpError = require('error').HttpError;
 module.exports = function(req, res, next) {
   if (!req.session.user) {
     //res.redirect('/');
-    return next(new HttpError(401, "Вы не авторизованы"));
+    return next(new HttpError(401, "you are not authorized to view this page!"));
     //return false;
   }
   next();
