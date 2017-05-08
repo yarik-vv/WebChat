a0641dab383c41b941c673790cc8833e360ecab6
Your Name <yarik.vashchuk@gmail.com>
2017-05-09 00:17:45 +0300

    test socket.io and socket.io-client

---
+++ test/websockets.js
@@ -0,0 +1,17 @@
+//create websocket server
+const ioserver = require('socket.io').listen(3000);
+ioserver.sockets.on('connection', (socket) => {});
+
+//import websocket client
+const io = require('socket.io-client');
+const url = 'http://127.0.0.1:3000';
+
+//test connection
+describe('Test socket.io and socket.io-client:', function(){
+  it('client connected to server', (done) => { 
+    io.connect(url)
+      .on('connect', () => {  
+        done();
+      })
+  });
+});
