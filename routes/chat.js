3298b667384cf9620c55eb68687d269ae719b239
yarik-vv <yarik.vashchuk@gmail.com>
2017-05-27 00:20:13 +0300

    small fixes route chat

--- routes/chat.js
+++ routes/chat.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const checkAuth = require('middleware/checkAuth');
 
-//check authorization
+//check user authorization
 router.use('/', checkAuth);
 
 //get to chat page
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('chat', {title: 'Travelchat'});
 });
 
