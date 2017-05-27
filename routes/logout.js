6b3f5b5746d0192e3a9a154631b7fdb93f8ec8b0
yarik-vv <yarik.vashchuk@gmail.com>
2017-05-28 00:33:01 +0300

    small updated logout route module

--- routes/logout.js
+++ routes/logout.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const log = require('libs/log')(module);
 
 //logout
 router.post('/', (req, res, next) => {
-  //log.info(req.user.username +' - sign out.');
 
   const sid = req.session.id;
   const io = req.app.get('io');
