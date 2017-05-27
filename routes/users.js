c105a8e0421144cfb6c1043690497a563ae217b7
yarik-vv <yarik.vashchuk@gmail.com>
2017-05-28 00:41:06 +0300

    review code users route module

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const User = require('models/user').User;
 const ObjectID = require('mongodb').ObjectID;
 const HttpError = require('error').HttpError;
@@ -7,12 +7,12 @@ const checkAdmin = require('middleware/checkAdmin');
 const log = require('libs/log')(module);
 
 
-//check admin authorization
+// check permission
 router.use('/', checkAdmin);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  User.find({}, function(err, users) {
+// get to users list
+router.get('/', (req, res, next) => {
+  User.find({}, (err, users) => {
     if(err){ 
       return next(err);
     }
@@ -20,7 +20,8 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/:id', function(req, res, next) {
+// get to selected user
+router.get('/:id', (req, res, next) => {
   if(!req.params.id) return next();
   try {
     let id = new ObjectID(req.params.id);
@@ -37,7 +38,8 @@ router.get('/:id', function(req, res, next) {
   }
 });
 
-router.post('/', function(req, res, next){
+// post request to delete selected user
+router.post('/', (req, res, next) => {
   log.debug('prinato post zapros s USERS page');
   const id = req.body.id;
 
