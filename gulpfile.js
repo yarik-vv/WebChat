a1326301bea5df6f394e8e3250b6f17f523c0b74
yarik-vv <yarik.vashchuk@gmail.com>
2017-07-07 07:49:26 +0300

    Gulp 4 + Webpack 2 = ♡

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const gulp = require('gulp');
+
+const webpack  = require('webpack');
+const webpackConfig = require('./frontend/webpack.config.js');
+let statsLog = {
+  colors: true,
+  reasons: true
+};
+
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 const sourcemaps = require('gulp-sourcemaps');
-const rename = require("gulp-rename");
+const notifier = require('node-notifier');
+const gutil    = require('gulp-util');
+const rename = require('gulp-rename');
 const gulpIf = require('gulp-if');
 const sass = require('gulp-sass');
 const del = require('del');
 
-gulp.task('clean', function() {
+gulp.task('clean', () => {
   return del([
     'views',
     'public/css', 
@@ -19,7 +29,7 @@ gulp.task('clean', function() {
   ]);
 });
 
-gulp.task('templates', function() {
+gulp.task('templates', () => {
   return gulp.src('frontend/**/*.jade')
     .pipe(rename({
       dirname: "views"
@@ -27,7 +37,7 @@ gulp.task('templates', function() {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', () => {
   return gulp.src('frontend/**/*.{woff,woff2}')
     .pipe(rename({
       dirname: "fonts"
@@ -35,7 +45,7 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('./public'));
 });
 
-gulp.task('images', function() {
+gulp.task('images', () => {
   return gulp.src('frontend/**/*.{jpg,png,svg}')
     .pipe(rename({
       dirname: "/"
@@ -43,7 +53,7 @@ gulp.task('images', function() {
     .pipe(gulp.dest('./public'));
 });
 
-gulp.task('saas', function() {
+gulp.task('saas', () => {
   return gulp.src('frontend/**/*.scss')
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))	
     .pipe(sass().on('error', sass.logError))
@@ -54,7 +64,34 @@ gulp.task('saas', function() {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('js', function() {
+gulp.task('webpack', (done) => {
+  webpack(webpackConfig, onComplete);
+  function onComplete(error, stats) {
+    if (error) {
+      onError(error);
+    } 
+    else if ( stats.hasErrors() ) { 
+      onError( stats.toString(statsLog) );
+    } 
+    else {
+      onSuccess( stats.toString(statsLog) );
+    }
+  }
+  function onError(error) {
+    let formatedError = new gutil.PluginError('webpack', error);
+    notifier.notify({ // чисто чтобы сразу узнать об ошибке
+      title: `Error: ${formatedError.plugin}`,
+      message: formatedError.message
+    });
+    done(formatedError);
+  }
+  function onSuccess(detailInfo) {
+    gutil.log('[webpack]', detailInfo);
+    done();
+  }
+});
+
+gulp.task('js', () => {
   return gulp.src('frontend/build/*.js')
     .pipe(rename({
       dirname: "js"
@@ -62,12 +99,12 @@ gulp.task('js', function() {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', gulp.series('clean', 'templates', 'fonts', 'images', 'saas', 'js'));
+gulp.task('build', gulp.series('clean', 'templates', 'fonts', 'images', 'saas', 'webpack', 'js'));
 
 gulp.task('dev', 
   gulp.series(
     'build',
-    gulp.parallel(function(){
+    gulp.parallel( () => {
       gulp.watch('frontend/**/*.jade', gulp.series('templates'));
       gulp.watch('frontend/**/*.{woff,woff2}', gulp.series('fonts'));
       gulp.watch('frontend/**/*.{jpg,png,svg}', gulp.series('images'));
