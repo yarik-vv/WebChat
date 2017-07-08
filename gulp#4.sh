957d54268574b9128f755391e7266bcf6f2ac2aa
yarik-vv <yarik.vashchuk@gmail.com>
2017-07-08 23:32:38 +0300

    remove Gulp 4 bash script, update MongoDB bash script

--- gulp#4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export PATH=./node_modules/.bin:$PATH
--- mongoDB.sh
+++ mongoDB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#uri for heroku = 'mongodb://yarik-vv:1@ds141950.mlab.com:41950/chat'
+# uri for heroku = 'mongodb://yarik-vv:1@ds141950.mlab.com:41950/chat'
 
 # for linux
 #sudo service mongod start
